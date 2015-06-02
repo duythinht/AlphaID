4711407f83111f556a250d5d1240e36b0ee9fe92
Thinh Tran <duythinht@gmail.com>
2015-06-02 14:30:31 +0700

    update setup

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ from distutils.core import setup
 setup(
     name='AlphaID',
     version='0.1',
+    author='Thinh Tran',
+    author_email='duythinht@gmail.com',
+    url='https://github.com/duythinht/AlphaID',
     packages=['alphaid',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
