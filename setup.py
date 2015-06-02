78b8006f737716cd14a1ec651010eed65c8cc141
Thinh Tran <duythinht@gmail.com>
2015-06-02 14:48:36 +0700

    Update install info

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 
 setup(
     name='AlphaID',
-    version='0.1',
+    version='0.1.1',
     author='Thinh Tran',
     author_email='duythinht@gmail.com',
     url='https://github.com/duythinht/AlphaID',
     packages=['alphaid',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
+    long_description='Python library to generate ID like youtube, twitter, tinyURL...',
 )
