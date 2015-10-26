f31056ccab90a4dffe2c1b5eed82b6e01a9e18aa
Thinh Tran <duythinht@gmail.com>
2015-10-26 10:41:11 +0700

    update test case

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import unittest
 from alphaid import AlphaID
 
 ALPHABET = 'IWKZCAF12345678'
@@ -7,3 +8,16 @@ aid = AlphaID(ALPHABET, LEN)
 
 string_id = aid.encode(2345) # WI4FA
 val_id = aid.decode(string_id) # 2345
+
+class TestAlphaID(unittest.TestCase):
+
+  def setUp(self):
+    self.aid = AlphaID(ALPHABET, LEN)
+
+  def testEncodeShouldBeCorrect(self):
+    string_id = self.aid.encode(2345)
+    self.assertEqual(string_id, 'WI4FA')
+
+  def testDecodeShouldBeCorrect(self):
+    int_id = self.aid.decode('WIAFA')
+    self.assertEqual(int_id, 1220)
